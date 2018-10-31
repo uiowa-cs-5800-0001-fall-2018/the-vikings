41daba7b2dc5442bf49275e233a518fd1f50fbb0
Hunza Zainab <hunzazainab@Hunzas-MacBook-Air.local>
2018-10-31 01:51:23 -0500

    Created Get_project route and database model for the table 'projects'

--- project/config.py
+++ project/config.py
@@ -1,4 +1,4 @@
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:the-vikings@localhost/the_vikings'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:the-vikings@104.248.121.89/the_vikings'
     DEBUG = True
     BCRYPT_LOG_ROUNDS = 4
--- project/database_controller/app.py
+++ project/database_controller/app.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from project.config import Config
 
+
 db = SQLAlchemy()
 app = Flask(__name__, static_url_path='')
 app.config.from_object(Config)
