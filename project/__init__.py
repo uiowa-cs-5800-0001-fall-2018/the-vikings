44614f53452c5be393fea6edb0c3926564ba6a94
Muhammed Ali Sit <masit@outlook.com>
2018-11-01 03:25:12 +0000

    authorization api, no ui yet

--- project/__init__.py
+++ project/__init__.py
@@ -1,11 +1,17 @@
+import sys
+sys.path.append("..")
+
 from flask import Flask, jsonify
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from config import Config
+from project.config import Config
 
 db = SQLAlchemy()
 app = Flask(__name__, static_url_path='')
 app.config.from_object(Config)
 bcrypt = Bcrypt(app)
 db.init_app(app)
+
+from project.auth.views import auth_blueprint
+app.register_blueprint(auth_blueprint)
